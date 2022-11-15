2ed85bd1d43b6996753679b4945bdc2f5caf7a56
PeterFlandin <peterflandin@gmail.com>
2022-11-15 10:58:02 +0100

    RouterNavigation

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
